d284103e9c4d9fee40563f10fee3c9a81d7350ed
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-06-07 23:58:08 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
+if (empty(env('APP_KEY'))) {
+    die('ERROR: APP_KEY is missing. Please set the application encryption key.');
+}
 
 // Load the Laravel application
 require __DIR__ . '/../vendor/autoload.php';
