a616cc1cfe6983bebaff8b94987bdced083e5609
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-06-09 01:14:15 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -63,3 +64,10 @@ Route::post('/register',[\App\Http\Controllers\AuthController::class, 'register'
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
 
 
+// Route tạm tạo symlink storage:link
+Route::get('/dev/storage-link', function () {
+    // Chỉ cho phép chạy nếu là môi trường local/dev hoặc đã xác thực admin
+    // Có thể thêm kiểm tra quyền tại đây nếu cần
+    Artisan::call('storage:link');
+    return 'Đã chạy xong php artisan storage:link!';
+});
