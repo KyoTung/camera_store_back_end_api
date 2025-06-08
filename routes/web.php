6b72e6c53daf230eb6189522c3b3609d945d3e4b
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-06-09 01:09:24 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,13 @@ Route::get('/', function () {
 Route::get('/db-test', function() {
     return DB::connection()->getDatabaseName();
 });
+use Illuminate\Support\Facades\Artisan;
+
+
+// Route tạm tạo symlink storage:link
+Route::get('/dev/storage-link', function () {
+    // Chỉ cho phép chạy nếu là môi trường local/dev hoặc đã xác thực admin
+    // Có thể thêm kiểm tra quyền tại đây nếu cần
+    Artisan::call('storage:link');
+    return 'Đã chạy xong php artisan storage:link!';
+});
