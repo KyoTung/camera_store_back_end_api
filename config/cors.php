91a6bdef24c216d5ff6563e0951e0c9449948c77
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-06-09 22:59:52 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,13 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'upload', 'temp-images'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://camera-store-frontend-kyotungs-projects.vercel.app'],
+    'allowed_origins' => [
+        'https://camera-store-frontend-kyotungs-projects.vercel.app',
+        'http://localhost:3000' // Sửa lại có http://
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
-
 ];
